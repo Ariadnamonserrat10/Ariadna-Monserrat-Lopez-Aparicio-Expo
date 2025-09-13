aef8e974c46cfc598a9626ac02fe73d6b893e190
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-09-12 23:46:26 -0600

    Primer contenido de la lista de alumnos

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Platform, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, Platform, StatusBar, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -13,6 +13,16 @@ const ListaAlumnos = () => {
                 <View style={{ width: 20 }}></View>
             </View>
             {/* Fin del AppBar */}
+            {/* Inicio del contenido (Lista de alumnos) */}
+            <ScrollView>
+                <Text style={style.TC}>Lista de Alumnos</Text>
+                <Image source={{ uri: 'https://i.pinimg.com/736x/a4/11/f9/a411f94f4622cfa7c1a87f4f79328064.jpg' }} />
+                <View style={style.cardinfo}>
+                    <Text style={style.username}>Ariadna Monserrat</Text>
+                    <Text style={style.userdetails}>Ing. en Sistemas Computacionales</Text>
+                </View>
+            </ScrollView>
+            {/* Fin del contenido (Lista de alumnos) */}
         </SafeAreaView>
     )
 }
@@ -24,7 +34,7 @@ const style = StyleSheet.create({
     main: {
         flex: 1,
         backgroundColor: '#cfaecfff',
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
+        // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 44,
     },
 
     appBar: {
@@ -38,9 +48,25 @@ const style = StyleSheet.create({
     },
     Title: {
         fontFamily: 'cursive',
-        fontSize: 20,   
+        fontSize: 20,
         fontWeight: 'bold',
         color: '#000000',
         textAlign: 'center',
+    },
+    TC: {
+        fontFamily: 'Arial',
+        fontSize: 25,   
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        color: '#000000',
+    },
+    cardinfo: {
+        backgroundColor: '#ffffffff',
+        margin: 20, 
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
     },
 });
